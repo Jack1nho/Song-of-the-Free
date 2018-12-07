8ac93cfc4e1a8b32a84a3794ad6f33dff5b4226e
lukens94 <44710933+lukens94@users.noreply.github.com>
2018-12-07 04:03:15 +0100

    Add files via upload

--- Project/js/lightbox.js
+++ Project/js/lightbox.js
@@ -1,4 +1,4 @@
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('myLightbox');
 var modalImg = document.getElementById("img01");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
