4dd0ea34a44deac60e02adbf40cef5e84b47fd30
lukens94 <44710933+lukens94@users.noreply.github.com>
2019-01-02 14:37:43 +0100

    Add files via upload

--- Project/js/slider-slick.js
+++ Project/js/slider-slick.js
@@ -1,41 +1,79 @@
 $(document).ready(function(){
     $('.lazy').slick({
-        dots: true,
-        infinite: true,
-        lazyLoad: 'ondemand',
-        slidesToShow: 4,
-        slidesToScroll: 2,
-        accessibility: true,
-        touchMove: true,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
+      dots: true,
+      infinite: true,
+      lazyLoad: 'ondemand',
+      slidesToShow: 4,
+      slidesToScroll: 2,
+      accessibility: true,
+      touchMove: true,
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-      });
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+
+    $('.slickHome').slick({
+      dots: true,
+      infinite: true,
+      lazyLoad: 'ondemand',
+      slidesToShow: 4,
+      slidesToScroll: 2,
+      accessibility: true,
+      touchMove: true,
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1.10,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
 
     $('.single-item').slick({
       infinite: true,
