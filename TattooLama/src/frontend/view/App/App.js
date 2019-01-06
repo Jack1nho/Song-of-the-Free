a448c92a1c31d0e56577982a38761245ece86fdf
Blackthunder93 <44710833+Blackthunder93@users.noreply.github.com>
2019-01-06 15:48:04 +0100

    Add files via upload

---
+++ TattooLama/src/frontend/view/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Result from '../Result/Result';
+
+class App extends Component {
+  constructor(){
+    super();
+  }
+  
+  render() {
+    return (
+      <div>
+        <Result></Result>
+      </div>
+    );
+  }
+}
+
+export default App;
