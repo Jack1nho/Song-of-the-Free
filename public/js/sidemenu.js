a0d2351b72afb53de71ba17f4e2eae9f25573c37
luca <lucavalenti94@gmail.com>
2019-02-20 21:28:16 +0100

    deleted files

--- public/js/sidemenu.js
+++
@@ -1,8 +0,0 @@
-var AltezzaElement = $(".sidebar").outerHeight();
-var AltezzaBox = $(".sticky-menu").outerHeight();
-var $window = $(window);
-var $body = $('body');
-var $windowHeight = $window.height();
-
-
-
--- public/js/slidedown-menu.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-    document.getElementById("top-sticky-menu").style.top = "0";
-  } else {
-    document.getElementById("top-sticky-menu").style.top = "-76px";
-  }
-}
