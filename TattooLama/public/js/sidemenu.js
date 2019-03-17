5f350a4a66df4bc980d4ac9e89aa6ee5ec70440b
Daniele Carta <crtdaniele@gmail.com>
2019-03-17 20:32:26 +0100

    Slick design

--- TattooLama/public/js/sidemenu.js
+++ TattooLama/public/js/sidemenu.js
@@ -3,10 +3,14 @@ setTimeout(function(){
     window.onbeforeunload = function() {
         //SLICK DESIGN IN TATTOER PAGE
         $(".tattoo_design .slick-slider .slick-dots li").each(function (index){ if(index == 0){ $(this).find("button").trigger("click") } });
+        $(".slickDesign.slick-slider .slick-dots li").each(function (index){ if(index == 0){ $(this).find("button").trigger("click") } });
     }
 
     //SLICK DESIGN IN TATTOER PAGE
     $(".tattoo_design .slick-slider .slick-dots li").each(function (index){ if(index == 0){ $(this).find("button").trigger("click") } });
+
+    //SLICK DESIGN IN DESIGN PAGE
+    $(".slickDesign.slick-slider .slick-dots li").each(function (index){ if(index == 0){ $(this).find("button").trigger("click") } });
 
     var AltezzaElement = $(".sidebar").outerHeight();
     var AltezzaBox = $("#sticky-menu").outerHeight();
--- TattooLama/src/frontend/view/Design/Tattooer_design.js
+++ TattooLama/src/frontend/view/Design/Tattooer_design.js
@@ -5,13 +5,43 @@ import SliderSlick from "react-slick";
 class TattooerDesign extends Component {
 
     render(){
+
+        var settings = {
+            dots: true,
+            infinite: true,
+            lazyLoad: 'ondemand',
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            accessibility: true,
+            touchMove: true,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true
+                    }
+                }
+                ]
+            };
+
         return(
             <div className="tattooer-design my-5">
                     <div className="text-3 mb-3">Altri Tattoo Design di: <span className="grey">{this.props.tattooer.name} {this.props.tattooer.surname}</span></div>
 
                     <div className="row row-grid">
+                    <SliderSlick {...settings} className="slickDesign">
                     {this.props.design_personal.slice(0,5).map((item, i) => {
-                        return <div className="col-6 col-sm-6 col-md-3 col-lg" key={i}>
+                        return <div className="col-xs-6 col-lg" key={i}>
                                     <Link className="grid" to={'/tatuaggio/' + item.url_design} >
                                         <img className="square" src={'../images/design/img/' + item.image} alt=""/>
                                             <h4 className="mt-2 mb-0 textArtista">{item.name_design}</h4>
@@ -20,6 +50,7 @@ class TattooerDesign extends Component {
                                 </div>  
                             }                   
                         )}
+                    </SliderSlick>
                     </div>
                 </div>
             );
