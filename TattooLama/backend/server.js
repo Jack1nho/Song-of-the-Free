373e5f25685a22e3fb3e25c9eede9144b1eaa414
Blackthunder93 <44710833+Blackthunder93@users.noreply.github.com>
2019-03-28 19:47:41 +0100

    Add files via upload

--- TattooLama/backend/server.js
+++ TattooLama/backend/server.js
@@ -24,6 +24,32 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//SENDINBLUE
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+var defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+// Configure API key authorization: api-key
+var apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = "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"
+// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+//apiKey.apiKeyPrefix['api-key'] = "Token"
+
+// Configure API key authorization: partner-key
+var partnerKey = defaultClient.authentications['partner-key'];
+partnerKey.apiKey = "YOUR API KEY"
+// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+//partnerKey.apiKeyPrefix['partner-key'] = "Token"
+
+var apiInstance = new SibApiV3Sdk.SMTPApi();
+
+var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+
+apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+  console.log('API called successfully. Returned data: ' + data);
+}, function(error) {
+  console.error(error);
+});
+
 //CONNECTION TO MONGODB
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
@@ -519,7 +545,7 @@ app.post('/getTattooDesign/:id', (req,res) => {
     
     }
     
-    //AGGIORNO COVER SE E CAMBIATA
+    //AGGIORNO IMG SE E CAMBIATA
     if (req.files.image !== undefined) {
 
         tattooimages.findOne({
